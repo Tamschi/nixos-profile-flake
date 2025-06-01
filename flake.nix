1065e355d97debd5c755c8658db4a723120633a2
Tamme Schichler <tamme@schichler.dev>
2025-06-01 09:22:47 +0000

    Refactor flake.nix to replace NixOS configurations with overlays for package management

--- flake.nix
+++ flake.nix
@@ -1,30 +1,20 @@
 {
-  description = "NixOS profile flake for common programs";
+  description = "NixOS overlay flake for common programs";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
-    nixosConfigurations = {
-      my-profile = let
-          pkgs = import nixpkgs { system = "x86_64-linux"; };
-        in nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            {
-              environment.systemPackages = with pkgs; [
-                vscode
-                discord
-                zulip
-                syncthingtray
-                florp
-                keepassxc
-              ];
-
-              services.syncthing.enable = true;
-              services.syncthing.tray = true;
-            }
-          ];
-        };
-    };
+    overlays = [
+      (final: prev: {
+        myPackages = with final; [
+          vscode
+          discord
+          zulip
+          syncthingtray
+          florp
+          keepassxc
+        ];
+      })
+    ];
   };
 }
