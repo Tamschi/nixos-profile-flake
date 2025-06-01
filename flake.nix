d4dc9af9649600da2538fa72eddf5f331c924ff0
Tamme Schichler <tamme@schichler.dev>
2025-06-01 09:27:49 +0000

    Added pkgs definition (manually) since GPT-4o didn't manage to do that.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,9 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: : let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+  in {
     overlays = [
       (final: prev: {
         myPackages = with final; [
