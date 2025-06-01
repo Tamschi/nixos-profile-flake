ee9180e5c1267dc996b3cd7f655b86ddd72152e5
Tamme Schichler <tamme@schichler.dev>
2025-06-01 09:29:12 +0000

    Actually nevermind, that wasn't it.

--- flake.nix
+++ flake.nix
@@ -3,9 +3,7 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: : let
-    pkgs = import nixpkgs { system = "x86_64-linux"; };
-  in {
+  outputs = { self, nixpkgs }: {
     overlays = [
       (final: prev: {
         myPackages = with final; [
